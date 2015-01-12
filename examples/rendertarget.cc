6e6f8d1a6fd34319dd96a8a4a8df70847fc4e24b
Dmitry Marakasov <amdmi3@amdmi3.ru>
2015-01-12 22:27:16 +0300

    Don't use deprecated method

--- examples/rendertarget.cc
+++ examples/rendertarget.cc
@@ -1,6 +1,6 @@
 /*
   libSDL2pp - C++11 bindings/wrapper for SDL2
-  Copyright (C) 2013-2014 Dmitry Marakasov <amdmi3@amdmi3.ru>
+  Copyright (C) 2013-2015 Dmitry Marakasov <amdmi3@amdmi3.ru>
 
   This software is provided 'as-is', without any express or implied
   warranty.  In no event will the authors be held liable for any damages
@@ -54,7 +54,7 @@ int Run() {
 
 	// Necessary checks according to SDL docs
 	SDL_RendererInfo ri;
-	render.GetInfo(&ri);
+	render.GetInfo(ri);
 
 	if (!(ri.flags & SDL_RENDERER_TARGETTEXTURE)) {
 		std::cerr << "Sorry, your renderer doesn't support texture targets" << std::endl;
