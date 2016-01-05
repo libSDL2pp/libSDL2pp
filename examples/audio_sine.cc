6000d165485fd54e378c6033e58d925d8d0837e5
Dmitry Marakasov <amdmi3@amdmi3.ru>
2016-01-05 17:04:17 +0300

    Include <cmath> for sin()/cos()

--- examples/audio_sine.cc
+++ examples/audio_sine.cc
@@ -1,6 +1,6 @@
 /*
   libSDL2pp - C++11 bindings/wrapper for SDL2
-  Copyright (C) 2013-2015 Dmitry Marakasov <amdmi3@amdmi3.ru>
+  Copyright (C) 2013-2016 Dmitry Marakasov <amdmi3@amdmi3.ru>
 
   This software is provided 'as-is', without any express or implied
   warranty.  In no event will the authors be held liable for any damages
@@ -19,6 +19,7 @@
   3. This notice may not be removed or altered from any source distribution.
 */
 
+#include <cmath>
 #include <iostream>
 
 #include <SDL2/SDL.h>
