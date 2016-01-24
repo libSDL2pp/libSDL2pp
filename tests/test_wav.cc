ed4d777f3987b40986be27eaa11b32c2941cdf79
Dmitry Marakasov <amdmi3@amdmi3.ru>
2016-01-24 22:57:16 +0300

    Add some AudioSpec tests to test_wav

--- tests/test_wav.cc
+++ tests/test_wav.cc
@@ -10,14 +10,24 @@ BEGIN_TEST(int, char*[])
 	Wav wav(TESTDATA_DIR "/test.wav");
 
 	{
+		// Wav tests
 		EXPECT_EQUAL(wav.GetLength(), (Uint32)121044);
 		EXPECT_TRUE(wav.GetBuffer() != nullptr);
-		EXPECT_TRUE((const Uint8*)wav.GetBuffer() == (Uint8*)wav.GetBuffer());
+		EXPECT_TRUE(wav.GetBuffer() == const_cast<const Wav&>(wav).GetBuffer());
 
 		const AudioSpec& spec = wav.GetSpec();
 
 		EXPECT_EQUAL(spec.Get()->freq, 44100);
 		EXPECT_EQUAL((int)spec.Get()->channels, 2);
+	}
+
+	{
+		const AudioSpec& spec = wav.GetSpec();
+
+		// Audiospec tests
+		AudioSpec spec2(spec.Get()->freq, spec.Get()->format, spec.Get()->channels, spec.Get()->samples);
+
+		EXPECT_TRUE(spec.IsSameFormat(spec2));
 	}
 
 	{
