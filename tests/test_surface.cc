b26842cf987c502139d6e87419a59d72c517ad6c
Dmitry Marakasov <amdmi3@amdmi3.ru>
2016-02-19 23:46:20 +0300

    Add Surface move test

--- tests/test_surface.cc
+++ tests/test_surface.cc
@@ -3,11 +3,14 @@
 #include <SDL2pp/Surface.hh>
 
 #include "testing.h"
+#include "movetest.hh"
 
 using namespace SDL2pp;
 
 BEGIN_TEST(int, char*[])
 	Surface crate(TESTDATA_DIR "/crate.png");
+
+	MOVE_TEST(Surface, crate, Get, nullptr);
 
 	{
 		EXPECT_EQUAL(crate.GetWidth(), 32);
