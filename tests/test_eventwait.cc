10c653b8158aa95f022b1b9a122fcad56fdfc6c9
Vraiment <jemc44@gmail.com>
2017-07-23 01:08:42 -0700

    Added event wait tests

--- tests/test_eventwait.cc
+++ tests/test_eventwait.cc
@@ -8,4 +8,34 @@ using namespace SDL2pp;
 using namespace SDL2pp::Event;
 
 BEGIN_TEST(int, char*[])
+	// Test wait event with no timeout
+	{
+		SDL_Event expected;
+		expected.type = SDL_USEREVENT;
+		expected.user.code = 99;
+		SDL_PushEvent(&expected);
+		
+		auto result = WaitEvent();
+		EXPECT_TRUE(result.type == expected.type);
+		EXPECT_TRUE(result.user.code == expected.user.code);
+	}
+
+	// Test wait event with timeout and with event
+	{
+		SDL_Event expected;
+		expected.type = SDL_USEREVENT;
+		expected.user.code = 32;
+		SDL_PushEvent(&expected);
+		
+		auto result = WaitEvent(10);
+		EXPECT_TRUE(!(result == NullOpt));
+		EXPECT_TRUE(result->type == expected.type);
+		EXPECT_TRUE(result->user.code == expected.user.code);
+	}
+
+	// Test wait event with timeout and no event
+	{
+		auto result = WaitEvent(10);
+		EXPECT_TRUE(result == NullOpt);
+	}
 END_TEST()
