04e10e978837e53be929cd2b55dd90d72cbb7af8
Vraiment <jemc44@gmail.com>
2017-07-31 00:37:19 -0700

    Added class to identify EventHandlerObjects with template metaprogramming

---
+++ SDL2pp/Private/EventHandlerObject.hh
@@ -0,0 +1,56 @@
+/*
+  libSDL2pp - C++11 bindings/wrapper for SDL2
+  Copyright (C) 2017 Vraiment <jemc44@gmail.com>
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+*/
+
+#ifndef SDL2PP_PRIVATE_EVENTHANDLEROBJECT_HH
+#define SDL2PP_PRIVATE_EVENTHANDLEROBJECT_HH
+
+#include <type_traits>
+
+namespace SDL2pp {
+/*
+ * This is code not to be used directly by users of SDL2pp.
+ */
+namespace Private {
+	/*
+	 * Templated class to identify a class that is not an event handler object.
+	 */
+	template <typename, typename, typename = void>
+	struct IsEventHandlerObject : std::false_type { };
+	
+	/*
+	 * Templated class to identify a class that is an event handler object, the
+	 * way this is done is by verifying that an instance of EventHandlerType has
+	 * the "HandleEvent" member function which received a "const EventType &"
+	 * and returns void.
+	 */
+	template <typename EventHandlerType, typename EventType>
+	struct IsEventHandlerObject<
+		EventHandlerType,
+		EventType,
+		std::is_same<
+			decltype(std::declval<EventHandlerType>().HandleEvent(std::declval<const EventType &>())),
+			void
+		>
+	> : std::true_type { };
+}
+}
+
+#endif
